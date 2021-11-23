d26b6661cd83bad334266065bc5bb5da0a836c37
SylarHugo <40252321+SylarHugo@users.noreply.github.com>
2021-11-23 15:11:01 +0800

    Add files via upload

---
+++ flasktest/templates/testjs.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML="JavaScript"; 
+
+
+$.ajax({
+  type: "GET",
+  url: "C:/Users/sylar/Desktop/mm.py",
+  data: { param: text}
+}).done(function( o ) {
+   // do something
+});
+}
